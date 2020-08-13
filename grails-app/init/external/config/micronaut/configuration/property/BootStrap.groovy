406e9c89a60776fe5a5b5f78d38205378cb4535b
Anders Aaberg <aaa@nine.dk>
2020-08-13 17:15:23 +0200

    Reproduced the problem correct...

--- grails-app/init/external/config/micronaut/configuration/property/BootStrap.groovy
+++ grails-app/init/external/config/micronaut/configuration/property/BootStrap.groovy
@@ -1,14 +1,19 @@
 package external.config.micronaut.configuration.property
 
+import org.springframework.beans.factory.annotation.Value
 import javax.inject.Inject
 
 class BootStrap {
+
+    @Value('${my.number}')
+    Integer myNumber
 
     @Inject
     MyConfiguration myConfiguration
 
     def init = { servletContext ->
-        assert myConfiguration.number == 42
+        assert myNumber == 42 // works both with external-config/my-external-config.yml and application.yml
+        assert myConfiguration.number == 42 // works only with application.yml
     }
     def destroy = {
     }
