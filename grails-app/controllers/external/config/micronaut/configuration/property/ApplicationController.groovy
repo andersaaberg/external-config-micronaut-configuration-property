311452650b73c57586bce4168aae8c68cbf07ab2
Anders Aaberg <aaa@nine.dk>
2020-08-13 16:23:33 +0200

    grails create-app external-config-micronaut-configuration-property --profile=rest-api --inplace

---
+++ grails-app/controllers/external/config/micronaut/configuration/property/ApplicationController.groovy
@@ -0,0 +1,14 @@
+package external.config.micronaut.configuration.property
+
+import grails.core.GrailsApplication
+import grails.plugins.*
+
+class ApplicationController implements PluginManagerAware {
+
+    GrailsApplication grailsApplication
+    GrailsPluginManager pluginManager
+
+    def index() {
+        [grailsApplication: grailsApplication, pluginManager: pluginManager]
+    }
+}
---
+++ grails-app/init/external/config/micronaut/configuration/property/BootStrap.groovy
@@ -0,0 +1,9 @@
+package external.config.micronaut.configuration.property
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
